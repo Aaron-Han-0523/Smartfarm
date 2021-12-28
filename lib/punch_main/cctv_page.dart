c47e8c4685600cda828d4fb67702f65331653b48
sherrygelato <sherrygelato@kakao.com>
2021-12-28 16:30:17 +0900

    KWHY-65 [+] edit cctv_page for multiple

--- lib/punch_main/cctv_page.dart
+++ lib/punch_main/cctv_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 
@@ -7,97 +8,84 @@ class CCTVPage extends StatefulWidget {
 }
 
 class _CCTVPageState extends State<CCTVPage> {
-  late VlcPlayerController playerController;
+  // late VlcPlayerController playerController;
+  late List<VlcPlayerController> playerControllers;
+  List<String> urls = [
+    'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+    'https://media.w3.org/2010/05/sintel/trailer.mp4',
+  ];
+
+  bool showPlayerControls = true;
 
   @override
   void initState() {
     super.initState();
-    playerController = VlcPlayerController.network(
-        "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
-        autoPlay: true);
+    playerControllers = <VlcPlayerController>[];
+    for (var i = 0; i < urls.length; i++) {
+      var controller = VlcPlayerController.network(
+        urls[i],
+        hwAcc: HwAcc.FULL,
+        autoPlay: false,
+        options: VlcPlayerOptions(
+          advanced: VlcAdvancedOptions([
+            VlcAdvancedOptions.networkCaching(2000),
+          ]),
+          rtp: VlcRtpOptions([
+            VlcRtpOptions.rtpOverRtsp(true),
+          ]),
+        ),
+      );
+      playerControllers.add(controller);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: SingleChildScrollView(
-          child: Column(children: [
-        Container(
-          alignment: Alignment.center,
-          // height: MediaQuery.of(context).size.height * 0.3,
-          color: Colors.grey[350], //색상변경 필요
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text("CCTV #1"),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          playerController.value.isPlaying
-                              ? playerController.pause()
-                              : playerController.play();
-                        });
-                      },
-                      child: const Text("Play/Pause"),
-                    ),
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.control_camera)),
-                  ],
-                ),
-                VlcPlayer(
-                  controller: playerController,
-                  aspectRatio: 16 / 9,
-                  placeholder: const Center(
-                    child: CircularProgressIndicator(),
+    return Container(
+      child: ListView.separated(
+        padding: EdgeInsets.all(8.0),
+        itemCount: playerControllers.length,
+        separatorBuilder: (_, index) {
+          return Divider(height: 50, thickness: 5);
+        },
+        itemBuilder: (_, index) {
+          return Container(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            alignment: Alignment.center,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("CCTV #${index + 1}"),
+                      IconButton(
+                          onPressed: () {
+                            setState(() {
+                              playerControllers[index].value.isPlaying
+                                  ? playerControllers[index].pause()
+                                  : playerControllers[index].play();
+                            });
+                          },
+                          icon: Icon(Icons.control_camera)),
+                    ],
                   ),
-                ),
-              ]),
-        ),
-        Container(
-          alignment: Alignment.center,
-          // height: MediaQuery.of(context).size.height * 0.3,
-          color: Colors.grey[350], //색상변경 필요
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text("CCTV #2"),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          playerController.value.isPlaying
-                              ? playerController.pause()
-                              : playerController.play();
-                        });
-                      },
-                      child: const Text("Play/Pause"),
-                    ),
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.control_camera)),
-                  ],
-                ),
-                VlcPlayer(
-                  controller: playerController,
-                  aspectRatio: 16 / 9,
-                  placeholder: const Center(
-                    child: CircularProgressIndicator(),
+                  VlcPlayer(
+                    controller: playerControllers[index],
+                    aspectRatio: 16 / 9,
                   ),
-                ),
-              ]),
-        ),
-      ])),
+                ]),
+          );
+        },
+      ),
     );
   }
 
   @override
-  void dispose() {
+  void dispose() async {
     super.dispose();
-    playerController.dispose();
+    for (final controller in playerControllers) {
+      await controller.dispose();
+    }
   }
 }
