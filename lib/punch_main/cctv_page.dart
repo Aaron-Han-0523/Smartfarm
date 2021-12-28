d160fcea2fa1a626acd2c9aa2e8afd2e4ad2a731
sherrygelato <sherrygelato@kakao.com>
2021-12-28 15:54:19 +0900

    KWHY-65 [+] edit cctv_page

--- lib/punch_main/cctv_page.dart
+++ lib/punch_main/cctv_page.dart
@@ -8,15 +8,6 @@ class CCTVPage extends StatefulWidget {
 
 class _CCTVPageState extends State<CCTVPage> {
   late VlcPlayerController playerController;
-  // final String urlToStreamVideo = 'https://media.w3.org/2010/05/sintel/trailer.mp4';
-  // final VlcPlayerController controller = VlcPlayerController.network(
-  //   'https://media.w3.org/2010/05/sintel/trailer.mp4',
-  //   hwAcc: HwAcc.FULL,
-  //   autoPlay: true,
-  //   options: VlcPlayerOptions(),
-  // );
-  // final int playerWidth = 640;
-  // final int playerHeight = 360;
 
   @override
   void initState() {
@@ -28,28 +19,79 @@ class _CCTVPageState extends State<CCTVPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        VlcPlayer(
-          controller: playerController,
-          aspectRatio: 16 / 9,
-          placeholder: const Center(
-            child: CircularProgressIndicator(),
-          ),
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SingleChildScrollView(
+          child: Column(children: [
+        Container(
+          alignment: Alignment.center,
+          // height: MediaQuery.of(context).size.height * 0.3,
+          color: Colors.grey[350], //색상변경 필요
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text("CCTV #1"),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          playerController.value.isPlaying
+                              ? playerController.pause()
+                              : playerController.play();
+                        });
+                      },
+                      child: const Text("Play/Pause"),
+                    ),
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.control_camera)),
+                  ],
+                ),
+                VlcPlayer(
+                  controller: playerController,
+                  aspectRatio: 16 / 9,
+                  placeholder: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              ]),
         ),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {
-              if (playerController.value.isPlaying) {
-                playerController.pause();
-              } else {
-                playerController.play();
-              }
-            });
-          },
-          child: const Text("Play/Pause"),
+        Container(
+          alignment: Alignment.center,
+          // height: MediaQuery.of(context).size.height * 0.3,
+          color: Colors.grey[350], //색상변경 필요
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text("CCTV #2"),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          playerController.value.isPlaying
+                              ? playerController.pause()
+                              : playerController.play();
+                        });
+                      },
+                      child: const Text("Play/Pause"),
+                    ),
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.control_camera)),
+                  ],
+                ),
+                VlcPlayer(
+                  controller: playerController,
+                  aspectRatio: 16 / 9,
+                  placeholder: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              ]),
         ),
-      ],
+      ])),
     );
   }
 
@@ -59,61 +101,3 @@ class _CCTVPageState extends State<CCTVPage> {
     playerController.dispose();
   }
 }
-
-// import 'package:video_player/video_player.dart';
-// import 'package:flutter/material.dart';
-//
-// class CCTVPage extends StatefulWidget {
-//   @override
-//   _CCTVPageState createState() => _CCTVPageState();
-// }
-//
-// class _CCTVPageState extends State<CCTVPage> {
-//   VideoPlayerController? _controller;
-// // const  _controller=VideoPlayerController;
-//   @override
-//   void initState() {
-//     super.initState();
-//     _controller = VideoPlayerController.network(
-//         'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
-//       ..initialize().then((_) {
-//         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-//         setState(() {});
-//       });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Video Demo',
-//       home: Scaffold(
-//         body: Center(
-//           child: _controller!.value.isInitialized
-//               ? AspectRatio(
-//             aspectRatio: _controller!.value.aspectRatio,
-//             child: VideoPlayer(_controller!),
-//           )
-//               : Container(),
-//         ),
-//         floatingActionButton: FloatingActionButton(
-//           onPressed: () {
-//             setState(() {
-//               _controller!.value.isPlaying
-//                   ? _controller!.pause()
-//                   : _controller!.play();
-//             });
-//           },
-//           child: Icon(
-//             _controller!.value.isPlaying ? Icons.pause : Icons.play_arrow,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-//
-//   @override
-//   void dispose() {
-//     super.dispose();
-//     _controller!.dispose();
-//   }
-// }
