36cad30e85d749c877b14609c1e9bdb9ab5b0352
sherrygelato <sherrygelato@kakao.com>
2021-12-29 18:12:54 +0900

    KWHY-67 [+] edit fullscreen for CCTV page

--- lib/punch_main/cctv_page.dart
+++ lib/punch_main/cctv_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
+
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
 
 /*
 * name : CCTV Page
@@ -24,84 +28,121 @@ class _CCTVPageState extends State<CCTVPage> {
     'https://media.w3.org/2010/05/sintel/trailer.mp4',
   ];
 
-  bool showPlayerControls = true;
+  late VideoPlayerController _videoPlayerController;
+  late ChewieController _chewieController;
   double _aspectRatio = 16 / 9;
 
-  // visibiliby
-  List<bool> visibility_value = [true, true];
-
   @override
-  void initState() {
+  initState() {
     super.initState();
-    playerControllers = <VlcPlayerController>[];
-    for (var i = 0; i < urls.length; i++) {
-      var controller = VlcPlayerController.network(
-        urls[i],
-        hwAcc: HwAcc.FULL,
-        autoPlay: true,
-        options: VlcPlayerOptions(
-          advanced: VlcAdvancedOptions([
-            VlcAdvancedOptions.networkCaching(2000),
-          ]),
-          rtp: VlcRtpOptions([
-            VlcRtpOptions.rtpOverRtsp(true),
-          ]),
-        ),
-      );
-      playerControllers.add(controller);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: ListView.separated(
-        padding: EdgeInsets.all(8.0),
-        itemCount: playerControllers.length,
-        separatorBuilder: (_, index) {
-          return Divider(height: 50, thickness: 5);
-        },
-        itemBuilder: (_, index) {
-          return Container(
-            padding: EdgeInsets.only(left: 10, right: 10),
-            alignment: Alignment.center,
-            child: Visibility(
-              visible: visibility_value[index],
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("CCTV #${index + 1}"),
-                        IconButton(
-                            onPressed: () {
-                              setState(() {
-                                playerControllers[index].value.isPlaying
-                                    ? playerControllers[index].pause()
-                                    : playerControllers[index].play();
-                              });
-                            },
-                            icon: Icon(Icons.control_camera)),
-                      ],
-                    ),
-                    VlcPlayer(
-                      controller: playerControllers[index],
-                      aspectRatio: _aspectRatio,
-                    ),
-                  ]),
-            ),
-          );
-        },
-      ),
+    _videoPlayerController = VideoPlayerController.network(urls[0]);
+    _chewieController = ChewieController(
+      allowFullScreen: true,
+      videoPlayerController: _videoPlayerController,
+      aspectRatio: _aspectRatio,
+      autoInitialize: true,
+      autoPlay: true,
     );
   }
 
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("CCTV #1"),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _chewieController.enterFullScreen();
+                    });
+                  },
+                  icon: Icon(Icons.control_camera)),
+            ],
+          ),
+          Container(
+            child: Chewie(
+              controller: _chewieController,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("CCTV #2"),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _chewieController.enterFullScreen();
+                    });
+                  },
+                  icon: Icon(Icons.control_camera)),
+            ],
+          ),
+          Container(
+            child: Chewie(
+              controller: _chewieController,
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     child: ListView.separated(
+  //       padding: EdgeInsets.all(8.0),
+  //       itemCount: playerControllers.length,
+  //       separatorBuilder: (_, index) {
+  //         return Divider(height: 50, thickness: 5);
+  //       },
+  //       itemBuilder: (_, index) {
+  //         return Container(
+  //           padding: EdgeInsets.only(left: 10, right: 10),
+  //           alignment: Alignment.center,
+  //           child: Visibility(
+  //             visible: visibility_value[index],
+  //             child: Column(
+  //                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //                 children: [
+  //                   Row(
+  //                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //                     children: [
+  //                       Text("CCTV #${index + 1}"),
+  //                       IconButton(
+  //                           onPressed: () {
+  //                             setState(() {
+  //                               playerControllers[index].value.isPlaying
+  //                                   ? playerControllers[index].pause()
+  //                                   : playerControllers[index].play();
+  //                             });
+  //                           },
+  //                           icon: Icon(Icons.control_camera)),
+  //                     ],
+  //                   ),
+  //                   VlcPlayer(
+  //                     controller: playerControllers[index],
+  //                     aspectRatio: _aspectRatio,
+  //                   ),
+  //                 ]),
+  //           ),
+  //         );
+  //       },
+  //     ),
+  //   );
+  // }
+
+  @override
   void dispose() async {
+    _videoPlayerController.dispose();
+    _chewieController.dispose();
     super.dispose();
-    for (final controller in playerControllers) {
-      await controller.dispose();
-    }
+    // for (final controller in playerControllers) {
+    //   await controller.dispose();
+    // }
   }
 }
