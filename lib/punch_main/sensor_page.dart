4dab5c28e2ae549e2ee90bb75603f30f0d64c6c5
sherrygelato <sherrygelato@kakao.com>
2021-12-30 14:32:51 +0900

    KWHY-76 [-] delete push notification code in sensor_page

--- lib/punch_main/sensor_page.dart
+++ lib/punch_main/sensor_page.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
