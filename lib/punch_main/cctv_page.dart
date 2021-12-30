8d27ab4b2c98645c2ee1b8f833de6f577f2b04de
sherrygelato <sherrygelato@kakao.com>
2021-12-30 11:36:13 +0900

    KWHY-67 [+] add visiblilty

--- lib/punch_main/cctv_page.dart
+++ lib/punch_main/cctv_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_vlc_player/flutter_vlc_player.dart';
-
 import 'package:chewie/chewie.dart';
 import 'package:video_player/video_player.dart';
 
@@ -20,10 +17,11 @@ class CCTVPage extends StatefulWidget {
 }
 
 class _CCTVPageState extends State<CCTVPage> {
-  late List<VlcPlayerController> playerControllers;
   List<String> urls = [
     'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
-    'https://media.w3.org/2010/05/sintel/trailer.mp4',
+    // 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+    "https://assets.mixkit.co/videos/preview/mixkit-daytime-city-traffic-aerial-view-56-large.mp4",
+    // "https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1226-large.mp4"
   ];
 
   late VideoPlayerController _videoPlayerController1;
@@ -31,6 +29,10 @@ class _CCTVPageState extends State<CCTVPage> {
   late VideoPlayerController _videoPlayerController2;
   late ChewieController _chewieController2;
   double _aspectRatio = 16 / 9;
+
+  // visibiliby
+  bool _visibility1 = true;
+  bool _visibility2 = true;
 
   @override
   initState() {
@@ -57,45 +59,67 @@ class _CCTVPageState extends State<CCTVPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("CCTV #1"),
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      // 카메라를 움직이는 버튼이나 일단 풀스크린전환
-                      _chewieController1.enterFullScreen();
-                    });
-                  },
-                  icon: Icon(Icons.control_camera)),
-            ],
-          ),
-          Container(
-            child: Chewie(
-              controller: _chewieController1,
+        child: Column(
+          children: [
+            Visibility(
+              visible: _visibility1,
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("CCTV #1"),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            // 카메라를 움직이는 버튼이나 일단 풀스크린전환
+                            _chewieController1.enterFullScreen();
+                          });
+                        },
+                        icon: Icon(Icons.control_camera)),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Chewie(
+                        controller: _chewieController1,
+                      ),
+                    ),
+                  ],
+                ),
+              ]),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("CCTV #2"),
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _chewieController2.enterFullScreen();
-                    });
-                  },
-                  icon: Icon(Icons.control_camera)),
-            ],
-          ),
-          Container(
-            child: Chewie(
-              controller: _chewieController2,
+            Visibility(
+              visible: _visibility2,
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("CCTV #2"),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            // 카메라를 움직이는 버튼이나 일단 풀스크린전환
+                            _chewieController2.enterFullScreen();
+                          });
+                        },
+                        icon: Icon(Icons.control_camera)),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Chewie(
+                        controller: _chewieController2,
+                      )
+                    ),
+                  ],
+                ),
+              ]),
             ),
-          ),
-        ]),
+          ],
+        ),
       ),
     );
   }
