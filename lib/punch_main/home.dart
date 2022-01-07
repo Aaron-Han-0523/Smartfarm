077d0ac1a05389e35df14a8e8a58f78f989fcd6c
sherrygelato <sherrygelato@kakao.com>
2022-01-07 15:03:23 +0900

    edit homePage for getdata

--- lib/punch_main/home.dart
+++ lib/punch_main/home.dart
@@ -5,6 +5,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:plms_start/punch_main/sensor_page.dart';
 import '../globals/stream.dart' as stream;
 
 import 'package:plms_start/punch_main/punch_main.dart';
@@ -55,64 +56,66 @@ class _HomeState extends State<Home> {
     stream.sensor_id = []; // pump1, pump2
     stream.sensorStatus = []; // pump1's on/off, pump2's on/off
 
-    getData;
+    getData();
     super.initState();
   }
 
   // getData
   Future<dynamic> getData() async {
-    // cctvs
-    final getCctvs = await dio.get('$url/$userId/site/$siteId/cctvs');
-    stream.cctvs = getCctvs.data;
-    print('##### homePage GET CCTV List from stream: ${stream.cctvs}');
-    print('##### homePage GET CCTV List length: ${stream.cctvs.length}');
-    stream.cctv_url = [];
-    for (var i = 0; i < stream.cctvs.length; i++) {
-      var cctvUrl = stream.cctvs[i]['cctv_url'];
-      stream.cctv_url.add(cctvUrl);
-    }
-    print('##### homePage GET CCTV Url List: ${stream.cctv_url}');
-
-    // pumps
-    final getPumps = await dio.get('$url/$userId/site/$siteId/controls/pumps');
-    stream.pumps = getPumps.data;
-    print('##### homePage GET Pumps LIST: ${stream.pumps}');
-    print('##### homePage Pumps LIST length: ${stream.pumps.length}');
-    stream.pump_name = [];
-    for (var i = 0; i < stream.pumps.length; i++) {
-      var pumpName = stream.pumps[i]['pump_name'];
-      stream.pump_name.add(pumpName);
-    }
-
-    // valves
-    final getValves =
-        await dio.get('$url/$userId/site/$siteId/controls/valves');
-    stream.valves = getValves.data;
-    print('##### homePage GET Valves LIST: ${stream.valves}');
-    print('##### homePage GET Valves LIST length: ${stream.valves.length}');
-    stream.valve_name = [];
-    for (var i = 0; i < stream.valves.length; i++) {
-      var valveName = stream.valves[i]['cctv_url'];
-      stream.valve_name.add(valveName);
-    }
-
-    // get pump1, pump2 = sensorId
-    final getSensorId = await dio.get('$url/$userId/site/$siteId/sensors');
-    stream.sensors = getSensorId.data;
-    print('##### homePage GET Sensors LIST: ${stream.sensors}');
-    print('##### homePage Sensors LIST length: ${stream.sensors.length}');
-    stream.sensor_id = [];
-    for (var i = 0; i < stream.sensors.length; i++) {
-      String sensorId = stream.sensors[i]['sensor_id'];
-      if (sensorId.contains('pump') == true) {
-        stream.sensor_id.add(sensorId);
-        print('$i) 이 sensorId($sensorId)는 pump가 맞습니다.');
-      } else {
-        print('$i) 이 sensorId($sensorId)는 pump가 아닙니다.');
+    if (mounted) {
+      // cctvs
+      final getCctvs = await dio.get('$url/$userId/site/$siteId/cctvs');
+      stream.cctvs = getCctvs.data;
+      print('##### homePage GET CCTV List from stream: ${stream.cctvs}');
+      print('##### homePage GET CCTV List length: ${stream.cctvs.length}');
+      stream.cctv_url = [];
+      for (var i = 0; i < stream.cctvs.length; i++) {
+        var cctvUrl = stream.cctvs[i]['cctv_url'];
+        stream.cctv_url.add(cctvUrl);
       }
-      print('##### homePage sensorId LIST: ${stream.sensor_id}');
-    }
+      print('##### homePage GET CCTV Url List: ${stream.cctv_url}');
 
+      // pumps
+      final getPumps =
+          await dio.get('$url/$userId/site/$siteId/controls/pumps');
+      stream.pumps = getPumps.data;
+      print('##### homePage GET Pumps LIST: ${stream.pumps}');
+      print('##### homePage Pumps LIST length: ${stream.pumps.length}');
+      stream.pump_name = [];
+      for (var i = 0; i < stream.pumps.length; i++) {
+        var pumpName = stream.pumps[i]['pump_name'];
+        stream.pump_name.add(pumpName);
+      }
+
+      // valves
+      final getValves =
+          await dio.get('$url/$userId/site/$siteId/controls/valves');
+      stream.valves = getValves.data;
+      print('##### homePage GET Valves LIST: ${stream.valves}');
+      print('##### homePage GET Valves LIST length: ${stream.valves.length}');
+      stream.valve_name = [];
+      for (var i = 0; i < stream.valves.length; i++) {
+        var valveName = stream.valves[i]['cctv_url'];
+        stream.valve_name.add(valveName);
+      }
+
+      // get pump1, pump2 = sensorId
+      final getSensorId = await dio.get('$url/$userId/site/$siteId/sensors');
+      stream.sensors = getSensorId.data;
+      print('##### homePage GET Sensors LIST: ${stream.sensors}');
+      print('##### homePage Sensors LIST length: ${stream.sensors.length}');
+      stream.sensor_id = [];
+      for (var i = 0; i < stream.sensors.length; i++) {
+        String sensorId = stream.sensors[i]['sensor_id'];
+        if (sensorId.contains('pump') == true) {
+          stream.sensor_id.add(sensorId);
+          print('$i) 이 sensorId($sensorId)는 pump가 맞습니다.');
+        } else {
+          print('$i) 이 sensorId($sensorId)는 pump가 아닙니다.');
+        }
+        print('##### homePage sensorId LIST: ${stream.sensor_id}');
+      }
+    }
     Get.offNamed('/sensor');
   }
 
